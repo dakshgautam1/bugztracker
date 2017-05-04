5e09e7dbc59dab63b30a605048c5c7c324527a56
dakshgautam <daksh.gautam@hashedin.com>
2017-05-04 11:11:43 +0530

    Template added

--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
 
 # Create your views here.
 def index(request):
-    from django.conf import settings
-    print (settings.BASE_DIR)
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    return render(request, 'index.html', { 'projects': projects})
